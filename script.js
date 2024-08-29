9717150f30e67974a1d4e00946cfa89a173f0214
clidnei <108068281+clidnei@users.noreply.github.com>
2024-08-29 19:52:03 -0300

    ss

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-reultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado:"Pergunta 1",
+        enunciado: "Pergunta 1",
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
     {
-        enunciado:"Pergunta 2",
+        enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-            ]
-    }    
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    }
 ];
 
 let atual = 0;
 let perguntaAtual;
 
-function mostrapergunta() {
-    perguntaAtual = perguntas [atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;   
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
-mostrapergunta();
+mostraPergunta();
